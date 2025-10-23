2b101a3ba14ec5c8aa94f415aa819dc87179aa53
Данни Титуанья <davila.tituanya@gazprombank.ru>
2025-10-23 23:14:24 +0300

    Unit tests for accounts-service working perfectly

--- accounts-service/src/test/java/io/github/danjos/mybankapp/TestMyBankAppApplication.java
+++ accounts-service/src/test/java/io/github/danjos/mybankapp/TestMyBankAppApplication.java
@@ -1,11 +1,12 @@
 package io.github.danjos.mybankapp;
 
+import io.github.danjos.mybankapp.accounts.AccountsServiceApplication;
 import org.springframework.boot.SpringApplication;
 
 public class TestMyBankAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(MyBankAppApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(AccountsServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
     }
 
 }
