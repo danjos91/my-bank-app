92dcaeda669b281950d546f2fc98b3b5cf2c39e9
danjos91 <danny_jtd@hotmail.com>
2025-12-10 02:00:25 +0300

    Add embedded Kafka integration tests and update notifications Helm test port

---
+++ accounts-service/src/test/java/io/github/danjos/mybankapp/accounts/kafka/KafkaNotificationProducerIntegrationTest.java
@@ -0,0 +1,71 @@
+package io.github.danjos.mybankapp.accounts.kafka;
+
+import io.github.danjos.mybankapp.accounts.AccountsServiceApplication;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = AccountsServiceApplication.class)
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, topics = {"account-created"})
+@TestPropertySource(properties = {
+        "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"
+})
+class KafkaNotificationProducerIntegrationTest {
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafka;
+
+    @Autowired
+    private KafkaNotificationProducer producer;
+
+    private Consumer<String, NotificationEvent> consumer;
+
+    @BeforeEach
+    void setUp() {
+        Map<String, Object> props = KafkaTestUtils.consumerProps("test-group", "true", embeddedKafka);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                org.springframework.kafka.support.serializer.JsonDeserializer.class);
+        props.put("spring.json.trusted.packages", "*");
+        consumer = new DefaultKafkaConsumerFactory<String, NotificationEvent>(props).createConsumer();
+        embeddedKafka.consumeFromAnEmbeddedTopic(consumer, "account-created");
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (consumer != null) {
+            consumer.close();
+        }
+    }
+
+    @Test
+    void publishAccountCreatedEvent_sendsMessageToKafka() {
+        producer.publishAccountCreatedEvent(123L, "ACC-1", "RUB");
+
+        var records = KafkaTestUtils.getRecords(consumer, 5000);
+        assertThat(records.count()).isGreaterThan(0);
+        var record = records.iterator().next();
+        assertThat(record.topic()).isEqualTo("account-created");
+        assertThat(record.key()).isEqualTo("123");
+        NotificationEvent event = (NotificationEvent) record.value();
+        assertThat(event.getUserId()).isEqualTo(123L);
+        assertThat(event.getNotificationType()).isEqualTo("ACCOUNT_CREATED");
+    }
+}
+
