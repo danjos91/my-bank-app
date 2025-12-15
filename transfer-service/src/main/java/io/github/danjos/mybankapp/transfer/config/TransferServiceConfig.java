382f4f631af417e34c26b72030ce9fc4e37480f0
danjos91 <danny_jtd@hotmail.com>
2025-12-16 00:25:42 +0300

    Use RestTemplateBuilder for automatic tracing in transfer-service

--- transfer-service/src/main/java/io/github/danjos/mybankapp/transfer/config/TransferServiceConfig.java
+++ transfer-service/src/main/java/io/github/danjos/mybankapp/transfer/config/TransferServiceConfig.java
@@ -3,6 +3,7 @@ package io.github.danjos.mybankapp.transfer.config;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
@@ -22,17 +23,17 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.Collections;
 
 @Configuration
 @Slf4j
 public class TransferServiceConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setInterceptors(Collections.singletonList(new JwtTokenInterceptor()));
-        return restTemplate;
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        // Use RestTemplateBuilder to preserve auto-configured interceptors (including tracing)
+        return builder
+                .additionalInterceptors(new JwtTokenInterceptor())
+                .build();
     }
 
     @Bean
