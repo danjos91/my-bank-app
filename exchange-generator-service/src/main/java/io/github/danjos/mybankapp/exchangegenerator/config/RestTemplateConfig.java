5710e263312a2665c14215ce98e4d62425c88f7d
danjos91 <danny_jtd@hotmail.com>
2025-12-16 00:26:23 +0300

    Use RestTemplateBuilder for automatic tracing in exchange-generator-service

--- exchange-generator-service/src/main/java/io/github/danjos/mybankapp/exchangegenerator/config/RestTemplateConfig.java
+++ exchange-generator-service/src/main/java/io/github/danjos/mybankapp/exchangegenerator/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package io.github.danjos.mybankapp.exchangegenerator.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,8 +9,9 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
     
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        // Use RestTemplateBuilder to preserve auto-configured interceptors (including tracing)
+        return builder.build();
     }
 }
 
